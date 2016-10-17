048a186aed666779045b37593bd9abe6a50fb87e
jeremy avnet <brainsik@users.noreply.github.com>
2016-10-17 00:06:27 -0700

    Add github release step

---
+++ ci/create-release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex -o pipefail
+env
+
+BASEDIR="$PWD"
+OUTPUT="$BASEDIR/release-data"
+
+cd $(mktemp -d)
+tar xfz ${BASEDIR}/package/json-store-*.tar.gz
+cd json-store-*
+python -c 'import setup; print setup.package_info["name"]' > ${OUTPUT}/name
+python -c 'import setup; print setup.package_info["version"]' > ${OUTPUT}/tag
